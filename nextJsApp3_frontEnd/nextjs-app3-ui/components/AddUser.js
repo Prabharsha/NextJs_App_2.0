01d824914f1f5ffd95ef36c9d968e6d1a51d3807
Prabharsha <pansiluprabharsha1@gamil.com>
2022-11-08 10:24:51 +0530

    save user ui completed

--- nextJsApp3_frontEnd/nextjs-app3-ui/components/AddUser.js
+++ nextJsApp3_frontEnd/nextjs-app3-ui/components/AddUser.js
@@ -1,5 +1,5 @@
 import { Dialog, Transition } from '@headlessui/react'
-import React, { Fragment,useState } from 'react'
+import React, { Fragment, useState } from 'react'
 
 const AddUser = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -15,7 +15,7 @@ const AddUser = () => {
         <>
             <div className="container mx-auto my-8">
                 <div className="h-12">
-                    <button onClick={openModel} className="rounded bg-green-400 text-white px-6 py-2 font-semibold hover:text-green-700"> + Add New User</button>
+                    <button onClick={openModel} className="rounded bg-green-400 text-white px-6 py-2 font-semibold hover:bg-green-600"> + Add New User</button>
                 </div>
             </div>
             <Transition appear show={isOpen} as={Fragment}>
@@ -30,6 +30,45 @@ const AddUser = () => {
                             leaveTo="opacity-0 scale-95">
                             <div className="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-md">
                                 <Dialog.Title as="h3" className="text-lg font-medium leading-6 text-gray-900">Add New User</Dialog.Title>
+                                <div className="flex max-w-md max-auto">
+                                    <div className="py-2 ">
+
+                                        {/* for NIC */}
+                                        <div className="h-14 my-4">
+                                            <label className="block text-gray-700 text-sm font-normal">
+                                                User NIC
+                                            </label>
+                                            <input type="text" name="userNIC" className="h-10 w-96 border mt-2 px-2 py2-"></input>
+                                        </div>
+
+                                        {/* for Name */}
+                                        <div className="h-14 my-4">
+                                            <label className="block text-gray-700 text-sm font-normal">
+                                                User Name
+                                            </label>
+                                            <input type="text" name="userName" className="h-10 w-96 border mt-2 px-2 py2-"></input>
+                                        </div>
+
+                                        {/* for contact */}
+                                        <div className="h-14 my-4">
+                                            <label className="block text-gray-700 text-sm font-normal">
+                                                Contact Number
+                                            </label>
+                                            <input type="text" name="userAddress" className="h-10 w-96 border mt-2 px-2 py2-"></input>
+                                        </div>
+
+                                        <div className="h-14 my-4 space-x-4 pt-4">
+                                        <button className=" rounded bg-green-500 text-white px-6 py-2 font-semibold hover:bg-green-600">
+                                            Register User
+                                        </button>
+
+                                        <button className="rounded bg-red-500 text-white px-6 py-2 font-semibold hover:bg-red-600">
+                                            cancel
+                                        </button>
+                                        </div>
+
+                                    </div>
+                                </div>
                             </div>
                         </Transition.Child>
                     </div>
